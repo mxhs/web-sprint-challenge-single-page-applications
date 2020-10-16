c107d94d6dc70c80bf1c97c101493a7b7a985075
mxhs <maxwellstofman@gmail.com>
2020-10-16 09:39:36 -0700

    Scaffolding And Route set up complete.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { Route } from 'react-router-dom';
+
+import Header from './Header'
+import HomePage from './HomePage'
+import Form from './Form'
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <Route exact path='/'>
+        <Header/>    
+          <h1>Lambda Eats</h1>
+          <p>You can remove this code and create your own header</p>
+        <HomePage/>
+      </Route>
+      <Route path ='/pizza'>
+        <Header/> 
+        <Form/>
+      </Route>
+    </div>
   );
 };
+
 export default App;
